37ef7311860ca4530f5dbb20490cdabc118e3f77
ham <ham@ham.com>
2022-12-16 12:48:18 -0800

    fixing github pages demo

--- demo/app.js
+++ demo/app.js
@@ -1,7 +1,7 @@
 import { BadPage, MainPage, ErrorPage, BasicPage, LoadedPage } from "./pages.js";
 
 q.route.style = 'browser';
-q.route.root = '/';
+q.route.root = window.location.host === 'bronkula.github.io' ? '/qjs/' : '/';
 q.route.init({
     routes:{
         "page/:route": BasicPage,
